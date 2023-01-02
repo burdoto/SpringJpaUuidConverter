7aeefc7e2e0b9b77ec8c419cb57b8377895b5f96
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-02 14:38:28 +0100

    return created IDs

--- src/main/java/com/example/springjpauuidconverter/TestController.java
+++ src/main/java/com/example/springjpauuidconverter/TestController.java
@@ -13,11 +13,29 @@ public class TestController {
     @Autowired
     public TestRepository repo;
 
+    public class UUIDs {
+        public final UUID Id1;
+        public final UUID Id2;
+
+        public UUID getId1() {
+            return Id1;
+        }
+
+        public UUID getId2() {
+            return Id2;
+        }
+
+        public UUIDs(UUID id1, UUID id2) {
+            Id1 = id1;
+            Id2 = id2;
+        }
+    }
+
     @GetMapping("/createEntities")
-    public String initEntities() {
-        repo.save(new TestEntity(UUID.randomUUID(), UUID.randomUUID(), "apple"));
-        repo.save(new TestEntity(UUID.randomUUID(), UUID.randomUUID(), "beanz"));
-        return "redirect:https://google.com";
+    public UUIDs initEntities() {
+        var ids = new UUIDs(UUID.randomUUID(), UUID.randomUUID());
+        repo.save(new TestEntity(ids.Id1, ids.Id2, "apple"));
+        return ids;
     }
 
     @GetMapping("/get1/{id}")
